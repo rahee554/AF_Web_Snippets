0752e6ea536bc7c710967765250a47d1ffe88787
Raheel Ahmad <76494683+rahee554@users.noreply.github.com>
2024-06-27 18:27:09 +0500

    Error Log Update

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# AF_Web_Snippets
- 
+# AF Web Snippets
+
+A handy and easy-to-use collection of web snippets for PHP Laravel to boost your web development and save a lot of time. This package includes AJAX form saving and much more.
+
+## Installation
+
+To install the package, use Composer:
+
+```sh
+composer require artflow-studio/snippets
+```
+
+
+
+## Usage
+
+Add the following line at the end of the <body> tag in your Blade template:
+```sh
+@stack('scripts')
+```
+And boom, you are done!
+## Features
+
+- Ajax Form Saving
+```
+ @AF_AjaxForm([
+        'id' => 'id_of_the_form',
+        'route' => 'route.of.the.form',
+        'method' => 'post',
+        'logType' => 'swal',
+        'onSuccess' => [
+            'log' => 'swal', // options: 'swal', 'alert', 'console'
+            'reload' => true, // true or false
+            'dtable' => 'myDataTable' // ID of the DataTable to reinitialize
+        ],
+    ])
+```
+
+
+## Authors
+
+- [@RaHee554](https://www.github.com/rahee554)
--- src/views/ajax_form.blade.php
+++ src/views/ajax_form.blade.php
@@ -1,40 +1,78 @@
 @push('scripts')
 <script>
     $(document).ready(function() {
-        // Assuming you have a global variable for environment set in your Blade template
+        // Global variables
         var env = '{{ app()->environment() }}';
-        // Assuming you have a global variable for logType set in your Blade template
         var logType = '{{ $logType ?? 'console' }}';
+        // Fetching the onSuccess configurations or setting default values
+        var onSuccess = @json($onSuccess ?? []);
+        var successLogType = onSuccess.log ?? 'alert';
+        var reloadOnSuccess = (onSuccess.reload ?? true) === true;
+        var dataTableId = onSuccess.dtable ?? '';
 
         $('#{{ $id ?? '' }}').submit(function(e) {
             e.preventDefault();
 
-            var formData = $(this).serialize();
+            var $form = $(this);
+            var $submitButton = $form.find('button[type="submit"]');
+            var formData = $form.serialize();
+
+            // Disable the submit button to prevent multiple submissions
+            $submitButton.prop('disabled', true);
 
             $.ajax({
                 url: '{{ route($route) }}',
                 type: '{{ strtoupper($method ?? 'POST') }}',
                 data: formData,
                 success: function(response) {
-                    console.log(response.message);
-                    // Handle success scenario (e.g., show success message)
-                    
+                    if (successLogType === 'swal') {
+                        Swal.fire({
+                            title: 'Success',
+                            text: response.message,
+                            icon: 'success'
+                        }).then((result) => {
+                            if (result.isConfirmed && reloadOnSuccess) {
+                                location.reload();
+                            } else if (result.isConfirmed && dataTableId) {
+                                $('#' + dataTableId).DataTable().ajax.reload();
+                            }
+                        });
+                    } else if (successLogType === 'alert') {
+                        alert(response.message);
+                        if (reloadOnSuccess) {
+                            location.reload();
+                        } else if (dataTableId) {
+                            $('#' + dataTableId).DataTable().ajax.reload();
+                        }
+                    } else if (env !== 'production') {
+                        console.log(response.message);
+                        if (reloadOnSuccess) {
+                            location.reload();
+                        } else if (dataTableId) {
+                            $('#' + dataTableId).DataTable().ajax.reload();
+                        }
+                    }
                 },
                 error: function(xhr) {
+                    // Re-enable the submit button in case of an error
+                    $submitButton.prop('disabled', false);
+
                     if (xhr.status === 422) {
                         var errors = xhr.responseJSON.errors;
+                        var errorMessage = Object.values(errors).map(function(error) {
+                            return '<p>' + error + '</p>';
+                        }).join('');
                         if (env !== 'production') {
                             if (logType === 'swal') {
                                 Swal.fire({
                                     title: 'Validation Error',
-                                    html: Object.values(errors).join('<br>'),
+                                    html: errorMessage,
                                     icon: 'error'
                                 });
                             } else {
                                 console.log(errors);
                             }
                         }
-                        // Handle validation errors (display to the user if needed)
                     } else {
                         var errorMessage = 'Error occurred. Please try again.';
                         if (env !== 'production') {
